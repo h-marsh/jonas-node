beff8bc37b09de1587fa257d15f27fdbb29d75e8
Hunter Marsh <marshsh@gmail.com>
2022-06-22 15:04:27 -0400

    async read and write files

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -8,7 +8,17 @@ const fs = require('fs');
 // console.log('File written!');
 
 // nonblocking, asynchronous (yay!!)
-fs.readFile('./txt/start.txt', 'utf-8', (error, data) => {
-	console.log(data);
+fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
+	if (error) return console.log('error~!');
+	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+		console.log(data2);
+		fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
+			console.log(data3);
+
+			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+				console.log('File has been created.');
+			});
+		});
+	});
 });
 console.log('Will read file...');
