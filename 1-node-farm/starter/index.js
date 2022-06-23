b7a2b93f72ff83599f6036090ab01cde11da2624
Hunter Marsh <marshsh@gmail.com>
2022-06-22 20:14:13 -0400

    built a simple web server with the built-in 'http' nodejs module, using the createServer() and listen() methods.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,24 +1,37 @@
 const fs = require('fs');
+const http = require('http');
 
-// blocking, synchronous technique
+////////////////////// sync and async read/write files //////////////////////
+
+// blocking, synchronous technique //
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
 // const textOut = `Some info: ${textIn}. \nCreated on ${Date.now()}`;
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File written!');
 
-// nonblocking, asynchronous (yay!!)
-fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
-	if (error) return console.log('error~!');
-	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
-		console.log(data2);
-		fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
-			console.log(data3);
+// nonblocking, asynchronous (yay!!) //
+// fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
+// 	if (error) return console.log('error~!');
+// 	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+// 		console.log(data2);
+// 		fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
+// 			console.log(data3);
 
-			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-				console.log('File has been created.');
-			});
-		});
-	});
+// 			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+// 				console.log('File has been created.');
+// 			});
+// 		});
+// 	});
+// });
+// console.log('Will read file...');
+
+////////////////////// a simple web server //////////////////////
+
+const server = http.createServer((req, res) => {
+	res.end('Hello from the server!');
 });
-console.log('Will read file...');
+
+server.listen(8000, '127.0.0.1', () => {
+	console.log('Server listening for requests on port 8000');
+});
