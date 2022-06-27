d2606db2ae4fe91ea64f967b3659b73bbbcca7eb
Hunter Marsh <marshsh@gmail.com>
2022-06-26 23:36:52 -0400

    using a bunch of replace() calls and a single map() call, it now replaces the placeholders with the data read from the data.json file when the page is requested.  the initial data is only read once, at the very beginning when the app is first started.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -30,20 +30,36 @@ const url = require('url');
 ////////////////////// a simple web server //////////////////////
 
 const templateOverview = fs.readFileSync(
-	`${__dirname}/template/template-overview.html`,
+	`${__dirname}/templates/template-overview.html`,
 	'utf-8'
 );
 const templateProduct = fs.readFileSync(
-	`${__dirname}/template/template-product.html`,
+	`${__dirname}/templates/template-product.html`,
 	'utf-8'
 );
 const templateCard = fs.readFileSync(
-	`${__dirname}/template/template-card.html`,
+	`${__dirname}/templates/template-card.html`,
 	'utf-8'
 );
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const parsedDataObject = JSON.parse(data);
+
+const replaceTemplate = function (template, product) {
+	let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRICE%}/g, product.price);
+	output = output.replace(/{%FROM%}/g, product.from);
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+
+	if (!product.organic)
+		output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+	return output;
+};
 
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
@@ -53,7 +69,14 @@ const server = http.createServer((req, res) => {
 		res.writeHead(200, {
 			'Content-type': 'text/html',
 		});
-		res.end(templateOverview);
+
+		const cardsHTML = parsedDataObject
+			.map((element) => replaceTemplate(templateCard, element))
+			.join('');
+
+		const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+
+		res.end(output);
 	}
 
 	// product page
