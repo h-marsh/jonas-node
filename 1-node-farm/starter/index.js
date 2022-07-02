a76923ed7ad56904138ab94597b623176966ca63
Hunter Marsh <marshsh@gmail.com>
2022-07-02 11:56:35 -0400

    setting module.exports equal to a function that is to be exported, then imported it elsewhere with require() and storing that in a variable to be used

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 ////////////////////// sync and async read/write files //////////////////////
 
 // blocking, synchronous technique //
@@ -44,22 +46,6 @@ const templateCard = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const parsedDataObject = JSON.parse(data);
-
-const replaceTemplate = function (template, product) {
-	let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-
-	if (!product.organic)
-		output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-	return output;
-};
 
 const server = http.createServer((req, res) => {
 	const { query, pathname } = url.parse(req.url, true);
